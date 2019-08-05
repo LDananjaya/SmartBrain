704f08502e7c9f1a1c99372af3ea3abf2560283b
LDananjaya <dananjaya703@gmail.com>
2019-08-05 17:30:15 +0530

    initial commit

---
+++ app/src/main/java/com/example/smartbrain/Main2Activity.java
@@ -0,0 +1,17 @@
+package com.example.smartbrain;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class Main2Activity extends AppCompatActivity {
+Button b5;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        b5.findViewById(R.id.button5);
+        b5.setText("Home");
+    }
+}
