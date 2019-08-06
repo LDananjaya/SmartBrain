87d263b5f1afad867735ad70b076ce608cb1e54c
minuraYehan <minura.yehan99@gmail.com>
2019-08-06 20:42:19 +0530

    Homepage,SignUp,EnglishSelection,EnglishEasy,EnglishMedium,EnglishHard

---
+++ app/src/main/java/com/example/smartbrain/EnglishSelection.java
@@ -0,0 +1,14 @@
+package com.example.smartbrain;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EnglishSelection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_english_selection);
+    }
+}
