24ff0d871fc93d42c981cd25d9f4a06dec59ba88
KelmiImashi <imashikelmi@gmail.com>
2019-08-06 21:09:27 +0530

    maths

---
+++ app/src/main/java/com/example/smartbrain/MathsSelection.java
@@ -0,0 +1,14 @@
+package com.example.smartbrain;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MathsSelection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maths_selection);
+    }
+}
