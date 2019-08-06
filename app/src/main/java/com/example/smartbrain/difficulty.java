c8635d8786921e1f3561744d6070814aef14346c
LDananjaya <dananjaya703@gmail.com>
2019-08-06 20:51:29 +0530

    dashboad updated

---
+++ app/src/main/java/com/example/smartbrain/difficulty.java
@@ -0,0 +1,48 @@
+package com.example.smartbrain;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class difficulty extends AppCompatActivity implements View.OnClickListener{
+    Button EasyBtn;
+    Button mediamBtn;
+    Button hardBtn;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.difficulty);
+
+        EasyBtn = findViewById(R.id.EasyBtn);
+        EasyBtn.setOnClickListener(this);
+
+        mediamBtn = findViewById(R.id.mediamBtn);
+        mediamBtn.setOnClickListener(this);
+
+        hardBtn = findViewById(R.id.hardBtn);
+        hardBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.EasyBtn:
+                Intent intent = new Intent(difficulty.this, question.class);
+                startActivity(intent);
+                break;
+
+            case R.id.mediamBtn:
+                Intent intent1 = new Intent(difficulty.this,question.class);
+                startActivity(intent1);
+                break;
+
+            case R.id.hardBtn:
+                Intent intent2 = new Intent(difficulty.this,question.class);
+                startActivity(intent2);
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/smartbrain/question2.java
@@ -0,0 +1,14 @@
+package com.example.smartbrain;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class question2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.question2);
+    }
+}
