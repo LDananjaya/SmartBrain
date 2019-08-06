68097312862346ed505ff50188df110b9f606404
minuraYehan <minura.yehan99@gmail.com>
2019-08-06 20:47:31 +0530

    Homepage,SignUp,EnglishSelection,EnglishEasy,EnglishMedium,EnglishHard

--- app/src/main/java/com/example/smartbrain/EnglishSelection.java
+++ app/src/main/java/com/example/smartbrain/EnglishSelection.java
@@ -2,7 +2,9 @@ package com.example.smartbrain;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class EnglishSelection extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class EnglishSelection extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_english_selection);
+    }
+
+    public void easyPage(View view) {
+        startActivity(new Intent(EnglishSelection.this,Englisheasy.class));
+    }
+
+    public void HardPage(View view) {
+        startActivity(new Intent(EnglishSelection.this,EnglishHard.class));
+    }
+
+    public void MediumPage(View view) {
+        startActivity(new Intent(EnglishSelection.this,EnglishMedium.class));
     }
 }
